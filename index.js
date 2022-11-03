9577804172901f1b48403712f510c9422032a108
kapil000001 <97395446+kapil000001@users.noreply.github.com>
2022-11-03 22:53:57 +0530

    updated

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const http = require("http");
+const http = require('http');
 
 
 const server = http.createServer((req,res) => {
-    res.end("index.html");
-})
+    console.log(req,url);
+    if (req.url == "/"){
+        res.end("Hello there");
+    }
+    else if (req.url == "/about"){
+        console.log("hello about us sides");
+    }
+    else if (req.url == "/Helpster"){
+        res.end("HEllo !! Welcome to our site");
+    }
+    else {
+        res.writeHead(404);
+        res.end("erroe 404: page not found");
+    }
+    res.end("server started");
+});
 
-server.listen(3000 , "127.0.0.1");
-console.log("success");
+server.listen(3000 , () => {
+    console.log("happy");
+});
+
+
+ 
