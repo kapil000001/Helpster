62d9ee4b8419c2458facb8f02a2711dce170c709
kapil000001 <97395446+kapil000001@users.noreply.github.com>
2022-11-04 00:02:51 +0530

    live server working

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const http = require('http')
 const fs = require('fs')
-const filecontent = fs.readFileSync(__dirname + "/index.html" ,"utf-8",(err) => {
-    if (err){
-        res.end("error : 404");
-    }
-    else res.end();
-});
+const filecontent = fs.readFileSync('index.html');
 
 const server = http.createServer((req,res) => {
-    res.writeHead(200 , {"content-type" : "text/html"})
-    res.end(filecontent)
-});
-
-server.listen(3000 , '127.0.0.1' , () => {
-    console.log("Listening on port 3000")
+    res.writeHead(200 , {'content-type' : 'text/html'});
+    res.end(filecontent);
 })
+
+server.listen(80, '127.0.0.1', () => {
+    console.log("Listening on port 80");
+});
 
 
  
