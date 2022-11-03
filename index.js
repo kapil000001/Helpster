88a8e894f7ba15611910654390ed35385c5283ee
kapil000001 <97395446+kapil000001@users.noreply.github.com>
2022-11-04 01:45:35 +0530

    updated index.js

--- index.js
+++ index.js
@@ -3,12 +3,43 @@ const fs = require('fs')
 const filecontent = fs.readFileSync('index.html');
 
 const server = http.createServer((req,res) => {
-    res.writeHead(200 , {'content-type' : 'text/html'});
-    res.end(filecontent);
-})
+    var reference = http.get(req,url);
 
-server.listen(80, '127.0.0.1', () => {
-    console.log("Listening on port 80");
+    res.writeHead(200 , {'content-type' : 'text/html'});
+    if (reference == '/'){
+        res.end(filecontent);
+    }
+    else if (reference == '/login'){
+        const filecontent = fs.readFile('login.html',(err,data) => {
+            if (err) {
+                res.end("error 404 : page not found");
+            }
+            else res.end(data);
+        });
+    }
+    else if (reference == '/register'){
+        const filecontent = fs.readFile('register.html',(err,data) => {
+            if (err) {
+                res.end("error 404 : page not found");
+            }
+            else res.end(data);
+        });
+    }else if (reference == '/about')
+    {
+        const filecontent = fs.readFile('about.html',(err,data) => {
+            if (err) {
+                res.end("error 404 : page not found");
+            }
+            else res.end(data);
+        });
+    }
+    else res.end(filecontent);
+    
+})
+const port = 6000;
+const hostname = '127.0.0.1';
+server.listen(port, hostname, () => {
+    console.log(`running at http://${hostname}:${port}/`);
 });
 
 
